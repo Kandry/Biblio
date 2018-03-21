db5321ce1c9671db3cd8117888d0100cc85205f4
Kandry <andry652227@yandex.ru>
2018-03-21 15:21:22 +0400

    create project

---
+++ biblio.sql
@@ -0,0 +1,188 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1
+-- Время создания: Мар 21 2018 г., 12:01
+-- Версия сервера: 10.1.31-MariaDB
+-- Версия PHP: 7.2.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `biblio`
+--
+CREATE DATABASE IF NOT EXISTS `biblio` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `biblio`;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `autor`
+--
+
+DROP TABLE IF EXISTS `autor`;
+CREATE TABLE `autor` (
+  `id` int(11) NOT NULL,
+  `name` varchar(30) CHARACTER SET utf8 DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- ССЫЛКИ ТАБЛИЦЫ `autor`:
+--
+
+--
+-- Дамп данных таблицы `autor`
+--
+
+INSERT INTO `autor` (`id`, `name`) VALUES
+(1, 'Dostoevsky'),
+(2, 'Strugatsky'),
+(3, 'Ivanov'),
+(4, 'Petrov'),
+(5, 'Sidorova'),
+(6, 'Frolenkov'),
+(7, 'Nassar');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `autors_books`
+--
+
+DROP TABLE IF EXISTS `autors_books`;
+CREATE TABLE `autors_books` (
+  `id` tinyint(4) NOT NULL,
+  `autor_id` int(11) DEFAULT NULL,
+  `book_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- ССЫЛКИ ТАБЛИЦЫ `autors_books`:
+--   `autor_id`
+--       `autor` -> `id`
+--   `book_id`
+--       `books` -> `id`
+--
+
+--
+-- Дамп данных таблицы `autors_books`
+--
+
+INSERT INTO `autors_books` (`id`, `autor_id`, `book_id`) VALUES
+(1, 1, 4),
+(2, 2, 5),
+(3, 3, 1),
+(4, 4, 1),
+(5, 5, 1),
+(6, 6, 1),
+(7, 3, 2),
+(8, 4, 2),
+(9, 7, 2),
+(10, 3, 3),
+(11, 4, 3),
+(12, 5, 3),
+(13, 6, 3),
+(14, 7, 3),
+(15, 5, 6),
+(16, 6, 6),
+(17, 7, 6);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `books`
+--
+
+DROP TABLE IF EXISTS `books`;
+CREATE TABLE `books` (
+  `id` int(11) NOT NULL,
+  `title` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- ССЫЛКИ ТАБЛИЦЫ `books`:
+--
+
+--
+-- Дамп данных таблицы `books`
+--
+
+INSERT INTO `books` (`id`, `title`) VALUES
+(1, 'Dr House'),
+(2, 'Issak Newton'),
+(3, 'Dr Freeman'),
+(4, 'Idiot'),
+(5, 'Hard to be a god'),
+(6, 'The book');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `autor`
+--
+ALTER TABLE `autor`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `autors_books`
+--
+ALTER TABLE `autors_books`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `autor_id_FK` (`autor_id`),
+  ADD KEY `book_id_FK` (`book_id`);
+
+--
+-- Индексы таблицы `books`
+--
+ALTER TABLE `books`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `autor`
+--
+ALTER TABLE `autor`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT для таблицы `autors_books`
+--
+ALTER TABLE `autors_books`
+  MODIFY `id` tinyint(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT для таблицы `books`
+--
+ALTER TABLE `books`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `autors_books`
+--
+ALTER TABLE `autors_books`
+  ADD CONSTRAINT `autor_id_FK` FOREIGN KEY (`autor_id`) REFERENCES `autor` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `book_id_FK` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ index.php
@@ -0,0 +1,104 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Отчёт книга-авторы</title>
+        <style>
+            body {
+                font-family: "Lucida Sans Unicode", "Lucida Grande", Sans-Serif;
+            }
+            #main {
+                position: relative;
+                top: 10px;
+                left: 10px; 
+            }
+            table {
+                padding-top: 15px;
+                font-size: 14px;
+                border-radius: 10px;
+                border-spacing: 0;
+                text-align: center;
+                height: auto;
+                width: auto;
+            }
+            th {
+                background: #BCEBDD;
+                color: white;
+                text-shadow: 0 1px 1px #2D2020;
+                padding: 10px 20px;
+            }
+            th, td {
+                border-style: solid;
+                border-width: 0 1px 1px 0;
+                border-color: white;
+            }
+            th:first-child, td:first-child {
+                text-align: left;
+            }
+            th:first-child {
+                border-top-left-radius: 10px;
+            }
+            th:last-child {
+                border-top-right-radius: 10px;
+                border-right: none;
+            }
+            td {
+                padding: 10px 20px;
+                background: #F8E391;
+            }
+            tr:last-child td:first-child {
+                border-radius: 0 0 0 10px;
+            }
+            tr:last-child td:last-child {
+                border-radius: 0 0 10px 0;
+            }
+            tr td:last-child {
+                border-right: none;
+            }
+        </style>
+    </head>
+    <body>
+        <div id="main">
+            <?php
+                $host = 'localhost'; // адрес сервера 
+                $database = 'biblio'; // имя базы данных
+                $user = 'root'; // имя пользователя
+                $password = 'soHardPass1234'; // пароль
+
+            // подключаемся к серверу
+                $link = new mysqli($host, $user, $password, $database); 
+                    if ($link -> connect_error) {
+                        die("Ошибка " . $link -> connect_error);
+                    }
+
+                 echo 'Подключение к базе данных.<br>';
+            
+                $result = $link ->query("SELECT b.title, COUNT(ab.id) FROM books b, autor a, autors_books ab WHERE b.id = ab.book_id AND a.id=ab.autor_id GROUP BY (ab.book_id) HAVING COUNT(ab.id) > 2 ORDER BY COUNT(ab.id) ");
+                echo "Количество строк: " . $result -> num_rows . "<br>";
+            ?>
+            <table>
+                <tr>
+                    <th>Title of book</th><th>How many autors</th>
+                </tr>
+
+                    <?php
+                        $result -> data_seek(0); 
+                        while($row = $result -> fetch_assoc())
+                        {
+                    ?>
+                            <tr>
+                                <td><?php echo $row["title"] ?></td>
+                                <td><?php echo $row["COUNT(ab.id)"] ?></td>
+                            </tr>
+                    <?php
+                        }
+
+                    ?>
+            <?php
+                $result -> close();
+                // закрываем подключение
+                $link -> close();
+            ?>
+            </table>
+        </div>
+    </body>
+</html>
